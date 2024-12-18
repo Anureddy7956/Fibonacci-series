2c6dd5dd113136507bb3fac1ff1f7489c3740a6d
Anureddy7956 <4q4947@gmail.com>
2024-12-18 19:37:24 +0530

    Fibonacci

---
+++ functions.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+//void calculateprice(float value);
+
+int main(){
+int n=4;
+printf("%f",pow(n,2));
+return 0;
+}
+//void calculateprice(float value){
+  //  value=value+(0.18*value);
+    //printf("final price is:%f\n,value");
+//}
